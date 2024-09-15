a8ba8c312a134b9e44559773ac3463e8357ab102
partha-protim-sarkar <parthaprotims681@gmail.com>
2024-09-15 18:04:31 +0530

    Customize Tailwind, setup ESLint, create component structure, and add assets

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    settings: { react: { version: '18.3' } },
+    env: {
+      browser: true,
+      es2020: true,
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
@@ -28,11 +24,17 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 0,
     },
+    settings: {
+      react: {
+        version: '18.2',
+      },
+    },
+    ignores: ['dist', 'eslint.config.js'],
   },
-]
+];
